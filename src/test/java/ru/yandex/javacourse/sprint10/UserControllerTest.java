3096d60046c62f7664c07dd20e5e93245f75be2d
Adil Tramov <adiltram420@gmail.com>
2025-11-01 00:05:01 +0300

    исправил тесты

--- src/test/java/ru/yandex/javacourse/sprint10/UserControllerTest.java
+++ src/test/java/ru/yandex/javacourse/sprint10/UserControllerTest.java
@@ -2,7 +2,7 @@ package ru.yandex.javacourse.sprint10;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-@AutoConfigureWebMvc
+@AutoConfigureMockMvc
 class UserControllerTest {
 
     @Autowired
