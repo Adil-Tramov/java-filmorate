2e78b6d9f76f2d0b0a6864372a686bb986041881
Adil Tramov <adiltram420@gmail.com>
2025-11-01 00:30:08 +0300

    удалил лишний import

--- src/main/java/ru/yandex/javacourse/sprint10/controller/UserController.java
+++ src/main/java/ru/yandex/javacourse/sprint10/controller/UserController.java
@@ -1,7 +1,6 @@
 package ru.yandex.javacourse.sprint10.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
