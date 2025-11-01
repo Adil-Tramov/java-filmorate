eafeba3be791e171d3f589611aa91cd791602a9b
Adil Tramov <adiltram420@gmail.com>
2025-11-01 17:20:56 +0300

    sprint-11

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,7 +1,56 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.service.FilmService;
+import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
+
+import java.util.Collection;
 
 @RestController
+@RequestMapping("/films")
 public class FilmController {
+    private final FilmStorage storage;
+    private final FilmService service;
+
+    @Autowired
+    public FilmController(FilmStorage storage, FilmService service) {
+        this.storage = storage;
+        this.service = service;
+    }
+
+    @GetMapping
+    public Collection<Film> all() { return storage.findAll(); }
+
+    @GetMapping("/{id}")
+    public Film get(@PathVariable long id) {
+        return storage.findById(id)
+                .orElseThrow(() -> new ValidationException("Фильм не найден"));
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Film create(@Valid @RequestBody Film film) { return storage.create(film); }
+
+    @PutMapping
+    public Film update(@Valid @RequestBody Film film) { return storage.update(film); }
+
+    @PutMapping("/{id}/like/{userId}")
+    public void addLike(@PathVariable long id, @PathVariable long userId) {
+        service.addLike(id, userId);
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public void removeLike(@PathVariable long id, @PathVariable long userId) {
+        service.removeLike(id, userId);
+    }
+
+    @GetMapping("/popular")
+    public Collection<Film> popular(@RequestParam(defaultValue = "10") int count) {
+        return service.popular(count);
+    }
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.filmorate.model;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+public class User {
+    private long id;
+    @Email @NotBlank private String email;
+    @NotBlank private String login;
+    private String name;
+    @Past private LocalDate birthday;
+    private final Set<Long> friends = new HashSet<>();
+}
