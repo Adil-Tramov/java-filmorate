20e2a37069cbc388b0ace4daea1d18ab999fb1a4
Adil Tramov <adiltram420@gmail.com>
2025-11-03 19:05:18 +0300

    добавлены пустые строки между объявлениями методов в интерфейсах хранилища

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -6,9 +6,14 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     Film create(Film film);
+
     Film update(Film film);
+
     Collection<Film> findAll();
+
     Optional<Film> findById(long id);
+
     void delete(long id);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -6,9 +6,14 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface UserStorage {
+
     User create(User user);
+
     User update(User user);
+
     Collection<User> findAll();
+
     Optional<User> findById(long id);
+
     void delete(long id);
 }
