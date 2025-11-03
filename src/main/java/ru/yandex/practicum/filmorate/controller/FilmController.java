7a89c21b5c67b84bf89683155ed2922826dad803
Adil Tramov <adiltram420@gmail.com>
2025-11-03 20:29:39 +0300

    удален import

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -19,7 +19,6 @@ import ru.yandex.practicum.filmorate.model.Film;
 
 import jakarta.validation.Valid;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
