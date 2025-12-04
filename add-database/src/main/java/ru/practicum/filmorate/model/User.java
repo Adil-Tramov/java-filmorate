96733ebdee8773c8d69461f76b4c00792a9f1fd7
Adil Tramov <adiltram420@gmail.com>
2025-12-05 00:12:20 +0300

    была нарушена правельность написание кода с }

--- add-database/src/main/java/ru/practicum/filmorate/model/User.java
+++ add-database/src/main/java/ru/practicum/filmorate/model/User.java
@@ -5,30 +5,62 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class User {
+
     private Long id;
     private String email;
     private String login;
     private String name;
     private LocalDate birthday;
-    private Set<Long> friends = new HashSet<Long>();
+    private Set<Long> friends = new HashSet<>();
 
-    public User() { }
+    public User() {
+    }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getLogin() { return login; }
-    public void setLogin(String login) { this.login = login; }
+    public String getEmail() {
+        return email;
+    }
 
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-    public LocalDate getBirthday() { return birthday; }
-    public void setBirthday(LocalDate birthday) { this.birthday = birthday; }
+    public String getLogin() {
+        return login;
+    }
 
-    public Set<Long> getFriends() { return friends; }
-    public void setFriends(Set<Long> friends) { this.friends = friends; }
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Set<Long> getFriends() {
+        return friends;
+    }
+
+    public void setFriends(Set<Long> friends) {
+        this.friends = friends;
+    }
 }
