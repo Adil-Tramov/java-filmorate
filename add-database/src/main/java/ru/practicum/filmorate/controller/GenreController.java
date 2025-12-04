db7c7f4c0464176ae47a8f12fdbc63af77b82a36
Adil Tramov <adiltram420@gmail.com>
2025-12-05 01:43:44 +0300

    исправление import

--- add-database/src/main/java/ru/practicum/filmorate/controller/GenreController.java
+++ add-database/src/main/java/ru/practicum/filmorate/controller/GenreController.java
@@ -1,6 +1,5 @@
 package ru.practicum.filmorate.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
