3dfb153ce91494bb096bddcfddb5639f1580c9dc
Adil Tramov <adiltram420@gmail.com>
2025-12-05 00:05:31 +0300

    в NotFoundException исправил фигурные скобки

--- add-database/src/main/java/ru/practicum/filmorate/exception/NotFoundException.java
+++ add-database/src/main/java/ru/practicum/filmorate/exception/NotFoundException.java
@@ -1,6 +1,8 @@
 package ru.practicum.filmorate.exception;
 
 public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) { super(message); }
+    public NotFoundException(String message) {
+        super(message);
+    }
 }
 
